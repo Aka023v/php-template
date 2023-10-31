b10c4691f95cbe3399c73ae6b95f775aa6388af4
Aka023v <148590386+Aka023v@users.noreply.github.com>
2023-10-31 21:38:21 +0900

    update my life

---
+++ docker/mysql/docker-entrypoint-initdb.d/sample.sql
@@ -0,0 +1,16 @@
+-- CREATE DATABASE IF NOT EXISTS contact;
+-- USE contact;
+
+-- CREATE TABLE IF NOT EXISTS `form`
+-- (
+--   `id`         int(11) NOT NULL AUTO_INCREMENT,
+--   `email`      varchar(255) NOT NULL,
+--   `content`    text,
+--   PRIMARY KEY (`id`)
+-- );
+
+-- INSERT INTO `form`(`email`, `content`) VALUES
+-- ('abe@gmail.com', お問い合わせテスト),
+-- ('ken@gmail.com', フォームテスト),
+-- ('yuto@gmail.com', 送信テスト),
+-- ('tabe@gmail.com', テストテスト);
